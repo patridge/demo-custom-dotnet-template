f216812b36deb4cd224e044d6b4bd32a3d672e8c
Adam Patridge <patridge@gmail.com>
2018-10-22 11:05:37 -0700

    Blog post 2 additions: input parameters.

---
+++ 2-input-parameters/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{helloMessage}");
+            Console.WriteLine("{helloMessageUpper}");
+        }
+    }
+}
